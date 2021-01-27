2c0712c0221fbeb47de29e7924c6b56da216a139
Andres Gomez <andresgomezagudelo@hotmail.com>
2021-01-26 23:11:26 -0500

    Encontrar coordinadores

---
+++ app.py
@@ -0,0 +1,64 @@
+from serial.tools import list_ports
+
+from digi.xbee.devices import ZigBeeDevice
+
+
+# Global constants
+PAN_ID = b'\x00\x00\x00\x00\x00\x00\x30\x06'
+ALLOWED_FIRMWARE_LOCAL = [
+    b'\x21\xA7'
+]
+
+# Flags
+coordinator_found = False
+
+
+# Funciones
+
+def pan_id_check(bytes1, bytes2):
+    """ 
+    Checks if the byte objects sent are equal
+    """
+
+    if bytes1 == bytes2:
+        return True
+
+    return False
+
+def firmware_check(firmware, list_firmwares):
+    """
+    Checks if the firmware is in list_firmwares
+    """
+    if firmware in list_firmwares:
+        return True
+
+    return False
+
+
+
+
+
+
+serial_port_list = [i.device for i in list_ports.comports()]
+
+for port in serial_port_list:
+
+    device = ZigBeeDevice(port, 9600)
+
+    try:
+        device.open()
+        local_pan_id = device.get_pan_id()
+        local_firmware = device.get_firmware_version()
+        if pan_id_check(local_pan_id, PAN_ID) and firmware_check(local_firmware, ALLOWED_FIRMWARE_LOCAL):
+            print(f'Coordinador encontrado en el puerto {port}')
+            coordinator_found = True
+            break
+
+    except:
+        continue
+
+
+if not coordinator_found:
+    print('No se encontro ningun xbee coordinador')
+
+
