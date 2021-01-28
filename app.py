fd451bce4e514f34aef4bc658fe7a23d5f044e96
Andres Gomez <andresgomezagudelo@hotmail.com>
2021-01-28 11:38:11 -0500

    modbus frame creator

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import time
+import libscrc
 
 from serial.tools import list_ports
 
@@ -38,6 +39,10 @@ def pan_id_check(bytes1, bytes2):
 def firmware_check(firmware, list_firmwares):
     """
     Checks if the firmware is in list_firmwares
+
+    Arguments:
+        firmware: bytes object
+        list_firmwares: list of allowed firmwares
     """
     if firmware in list_firmwares:
         return True
@@ -48,7 +53,10 @@ def firmware_check(firmware, list_firmwares):
 def find_xbee_coordinator(serial_port_list):
 
     """
-    Finds a xbee coordinator, aand open its communication interface
+    Finds a xbee coordinator, and open its communication interface
+
+    Arguments: 
+        serial_port_list: list of available serial ports
     """
 
     global PAN_ID, ALLOWED_FIRMWARE_LOCAL
@@ -82,7 +90,16 @@ def callback_discovery_finished(status):
         print(f"Error al descubrir la red: {status.description}")
 
 
-def find_xbee_network(device):
+def find_xbee_network(device, callback_device_discovered, callback_discovery_finished):
+
+    """
+    Creates a thread searching for xbees on network
+
+    Arguments:
+        device: Local xbee object
+        callback_device_discovered(remote_xbee_object): function to execute when a xbee is found
+        callback_discovery_finished(network_discovery_status): function to execute when discovery process is finished
+    """
 
     xbee_network = device.get_network()
     xbee_network.set_discovery_timeout(5)
@@ -105,6 +122,36 @@ def find_xbee_network(device):
 
 
 
+def crc_modbus(modbus_frame):
+    """
+    Arguments:
+        modbus_frame: bytes object
+    """
+
+    crc = libscrc.modbus(modbus_frame)
+    print(crc.hex())
+    crc16 = crc.to_bytes(2, byteorder='little')
+    print(crc16.hex())
+
+
+
+
+def create_modbus(address, command, reg_address, data_16):
+    """
+    address = b'\x01'
+    command = b'\x06'
+    reg_address = b'\x10\x00' (most - less)
+    data_16 = b'\x00\xFF' (most - less)
+    crc = b'\xCD\x4A'(less - most)
+    """
+    
+    modbus_frame = address + command + reg_address + data_16
+
+
+
+
+
+
 
 
 
@@ -117,10 +164,15 @@ device = find_xbee_coordinator(serial_port_list)
 if device:
     print(f'Se encontro un xbee coordinador con pan id: {device.get_pan_id().hex()}')
 
-    xbee_network = find_xbee_network(device)
+    xbee_network = find_xbee_network(device, callback_device_discovered, callback_discovery_finished)
     xbee_maquina1 = xbee_network.get_device_by_node_id(NODOS['maquina1'])
 
     device.send_data(xbee_maquina1, MODBUS_WRITE)
+
+
+    address = b'\x01\x06\x10\x00\x00\xFF\xCD\x4A'
+
+    crc_modbus(address)
 
 
 
